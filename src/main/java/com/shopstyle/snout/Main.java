d6c6e47908b84886032589f903350d64db259e9c
Benjamin Possolo <bpossolo@gmail.com>
2015-01-21 13:26:10 -0800

    fixed usage statement

--- src/main/java/com/shopstyle/snout/Main.java
+++ src/main/java/com/shopstyle/snout/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		if (args.length != 2){
-			System.out.println("Usage: snout <locale> <config-filename> <tests-filename>");
+			System.out.println("Usage: main.sh <config-filename> <tests-filename>");
 			System.exit(InsufficientArgs);
 		}
 		File configFile = new File(args[0]);
